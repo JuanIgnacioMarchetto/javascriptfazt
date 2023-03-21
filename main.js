f6312a8ae41467afade6d6efe6dca0deeb0605d3
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-03-21 07:36:35 -0300

    21/2/2023

---
+++ main.js
@@ -0,0 +1,86 @@
+/*console.log ('hola mundo')
+
+document.write("<h1>Hola mundo</h1>")
+
+   console.error('mi error')
+
+   console.error('app fallida')
+*/
+/*
+//tiposde datos
+
+
+//string
+"hala mundo"
+'hola mundo'//string
+
+// Number
+10000000
+   - 2.3// number
+
+//boolean
+true
+false//bolean
+
+//array
+
+['joe', 'ryan', 'marcha']
+[1.2, 3]
+[false, true]
+
+//Object
+[
+   {
+      "username": 'ryan',
+      "score": 70.4,
+      "hours": 14,
+      "proffesional": true,
+   }]
+[
+{
+   "username": 'joe',
+   "score": 20,
+    "hours": 1,
+   "proffesional": false,
+   }
+   
+   ]
+   */
+
+   /*
+console.log("this is a string");
+
+console.log(14,3),
+
+console.log(true),
+
+console.log([1,2,3]);
+
+console.log(
+   {
+      "username": 'ryan',
+      "score": 70.4,
+      "hours": 14,
+      "proffesional": true,
+   })
+*/
+/*
+var nameuser="john";
+let lastname="carter";
+
+console.log(lastname);
+console.log(nameuser);
+
+var fullname=(lastname+' '+ nameuser);
+
+nameuser="pepe",
+
+console.log(nameuser)
+
+console.log(fullname)
+
+const PI = 3,1415;
+*/
+
+
+
